44df08bcfacc76dd180db1eb68b7646f83ea2bff
dominikpegler <dinkopeg@posteo.net>
2022-09-24 23:06:44 +0200

    added postgres config

--- api/database.py
+++ api/database.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import json
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./psiz-collect.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./psiz-collect.db"
+with open("config.json") as fp:
+    config = json.load(fp)
+SQLALCHEMY_DATABASE_URL = config["SQLALCHEMY_DATABASE_URL"]
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    # connect_args={"check_same_thread": False} # for sqlite
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
