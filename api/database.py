455b4bed58ef553b589a4c8fe3b5394226e6cfb6
dominikpegler <dinkopeg@posteo.net>
2022-09-30 14:11:22 +0200

    grab sqlalchemy postgres from env if available

--- api/database.py
+++ api/database.py
@@ -2,11 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import json
+import os
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./psiz-collect.db"
-with open("config.json") as fp:
-    config = json.load(fp)
-SQLALCHEMY_DATABASE_URL = config["SQLALCHEMY_DATABASE_URL"]
+
+if os.environ.get("SQLALCHEMY_DATABASE_URL") == None:
+    with open("config.json") as fp:
+        config = json.load(fp)
+    SQLALCHEMY_DATABASE_URL = config["SQLALCHEMY_DATABASE_URL"]
+else:
+    SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
