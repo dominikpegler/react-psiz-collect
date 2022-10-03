75f0cde12eceee6a15cdb9161d865368325ae1ff
dominikpegler <dinkopeg@posteo.net>
2022-10-03 21:08:36 +0200

    changed SERVER_URL to heroku

--- static/js/App.js
+++ static/js/App.js
@@ -2,7 +2,7 @@
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var SERVER_URL = "http://127.0.0.1:5000";
+var SERVER_URL = "https://roastpsiz-api.herokuapp.com";
 
 var App = function App() {
   var _React$useState = React.useState(),
