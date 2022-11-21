58418d0760cbad8c9eed16b0ee76818100045a74
dominikpegler <dinkopeg@posteo.net>
2022-11-21 18:01:08 +0100

    serve -l 80 added

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #uvicorn api.main:app --proxy-headers --host 0.0.0.0 --port 5000 & serve -l 80
 
 # for production
-gunicorn api.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
+gunicorn api.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000 & serve -l 80
