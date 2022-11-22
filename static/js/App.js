b9c9ffcb3bbebe5e423f6c90471a7ee6410bb0e2
userone <userone@psycyb-portia.psy.univie.ac.at>
2022-11-22 16:13:36 +0100

    removed async fetch

--- static/js/App.js
+++ static/js/App.js
@@ -1,10 +1,4 @@
-import _regeneratorRuntime from "babel-runtime/regenerator";
-
-var _this = this;
-
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 var SERVER_URL = "http://psycyb-portia.psy.univie.ac.at:5000";
 
@@ -29,43 +23,6 @@ var App = function App() {
       setWorkerId(e.target.value);
     }
   };
-
-  // TODO USE A BUNDLER! NO MORE CDNs
-  // also check if the solid app has some calls to non-public ports (only visible inside the docker network). if yes, check how it was done there
-  var handleFetchData = function () {
-    var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee() {
-      var response, data;
-      return _regeneratorRuntime.wrap(function _callee$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-              _context.next = 2;
-              return fetch(SERVER_URL + "/test-backend-connection/", {
-                mode: "cors"
-              });
-
-            case 2:
-              response = _context.sent;
-              _context.next = 5;
-              return response.json();
-
-            case 5:
-              data = _context.sent;
-
-              console.log(data);
-
-            case 7:
-            case "end":
-              return _context.stop();
-          }
-        }
-      }, _callee, _this);
-    }));
-
-    return function handleFetchData() {
-      return _ref.apply(this, arguments);
-    };
-  }();
 
   var testConnection = function testConnection() {
     fetch(SERVER_URL + "/test-backend-connection/").then(function (response) {
