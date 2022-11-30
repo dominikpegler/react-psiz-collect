1354caea3b782679369e97ee9ecb81a893c99b83
dominikpegler <dinkopeg@posteo.net>
2022-12-01 00:10:11 +0100

    added pagination

--- src/Survey.js
+++ src/Survey.js
@@ -2,7 +2,9 @@
 
 const Survey = ({ workerId }) => {
   {
-    const [survey, setSurvey] = React.useState([]);
+    const [survey, setSurvey] = React.useState();
+    const [pageNo, setPageNo] = React.useState(0);
+    const [pages, setPages] = React.useState();
     const [assignmentId, setAssignmentId] = React.useState();
     const [beginHit, _] = React.useState(new Date());
     const [startMs, setStartMs] = React.useState(new Date());
@@ -11,6 +13,14 @@ const Survey = ({ workerId }) => {
     const [selection, setSelection] = React.useState();
 
     const QUESTIONS_PP = 6;
+
+    const handlePagination =(move)=>{
+      if ((pageNo + move)>=0 & (pageNo + move)<pages.length) {
+        setPageNo(pageNo+move)
+      } else {
+        console.log("Won't do nothing.")
+      }
+    }
 
     const downloadSurveyData = (projectId) => {
       fetch(SERVER_URL + "/get-surveys-by-project-id/" + projectId, {
@@ -34,39 +44,71 @@ const Survey = ({ workerId }) => {
         });
     };
 
-    // runs once at the beginning of the assignment
+    // runs only once at the beginning
     React.useEffect(() => {
       downloadSurveyData(projectId);
     }, []);
 
+
+    // runs when survey data changes
+    React.useEffect(() => {
+      if (survey !== undefined) {
+        let i = 0;
+        let j;
+        let nItems = 0;
+        let nPages;
+        let len = survey.length;
+        let pagesTmp = []
+        for (; i < len; ) {
+          nItems = survey[i]["items"].length;
+          nPages = Math.ceil(nItems / QUESTIONS_PP);
+          j = 0;
+          for (; j < nPages; ) {
+            pagesTmp.push([i, QUESTIONS_PP*j]);
+            j++;
+          }
+          i++;
+        } 
+          setPages(pagesTmp)
+      }
+    }, [survey]);
+    
     // rendering
     return (
       <div>
         <div className={"container"}>
+          <h1>{pages && survey[pages[pageNo][0]]["name"]}</h1>
           <div>
-            {survey &&
-              survey.map(
-                (s) =>
-                  s && (
-                    <div>
-                      <h1>{s.name}</h1>
-                      {s.items &&
-                        s.items.map(
-                          (item) =>
-                            item && (
-                              <div>
-                                <span>{item}</span>
-                                <ResponseBox s={s}  item={item}/>
-                              </div>
-                            )
-                        )}
-                    </div>
-                  )
-              )}
+            {pages &&
+              survey[pages[pageNo][0]]["items"]
+                .slice(pages[pageNo][1], pages[pageNo][1] + QUESTIONS_PP)
+                .map((item) => (
+                  <div>
+                    <span>{item}</span>
+                    <ResponseBox
+                      s={survey[pages[pageNo][0]]}
+                      item={item}
+                    />
+                  </div>
+                ))}
+            {1 + pageNo}/{pages && pages.length}
           </div>
           <div className={"bottom-tile"} style={{ justifyContent: "center" }}>
             <div className={"submit-button-tile"}>
-              <button onClick={(e) => handleClick(e)}>Proceed</button>
+              <button
+                disabled={pageNo === 0}
+                onClick={() => handlePagination(-1)}
+              >
+                Back
+              </button>
+            </div>
+            <div className={"submit-button-tile"}>
+              <button
+                disabled={pages && pageNo == pages.length - 1}
+                onClick={() => handlePagination(1)}
+              >
+                Next
+              </button>
             </div>
           </div>
         </div>
@@ -85,10 +127,10 @@ const ResponseBox =({s, item})=> {
         justifyContent: "space-between",
         alignItems: "flex-end",
         marginTop: "1rem",
-        marginBottom: "3rem"
+        marginBottom: "2rem"
       }}
     >
-      {s.scale.map(
+      {s.scale && s.scale.map(
         (likert) =>
           likert && (
             <div
@@ -98,7 +140,6 @@ const ResponseBox =({s, item})=> {
               }}
             >
               <label for={item + String(likert.value)}>{likert.label}</label>
-
               <input
                 type={"radio"}
                 id={item + String(likert.value)}
--- static/js/Survey.js
+++ static/js/Survey.js
@@ -6,39 +6,57 @@ var Survey = function Survey(_ref) {
   var workerId = _ref.workerId;
 
   {
-    var _React$useState = React.useState([]),
+    var _React$useState = React.useState(),
         _React$useState2 = _slicedToArray(_React$useState, 2),
         survey = _React$useState2[0],
         setSurvey = _React$useState2[1];
 
-    var _React$useState3 = React.useState(),
+    var _React$useState3 = React.useState(0),
         _React$useState4 = _slicedToArray(_React$useState3, 2),
-        assignmentId = _React$useState4[0],
-        setAssignmentId = _React$useState4[1];
+        pageNo = _React$useState4[0],
+        setPageNo = _React$useState4[1];
 
-    var _React$useState5 = React.useState(new Date()),
+    var _React$useState5 = React.useState(),
         _React$useState6 = _slicedToArray(_React$useState5, 2),
-        beginHit = _React$useState6[0],
-        _ = _React$useState6[1];
+        pages = _React$useState6[0],
+        setPages = _React$useState6[1];
 
-    var _React$useState7 = React.useState(new Date()),
+    var _React$useState7 = React.useState(),
         _React$useState8 = _slicedToArray(_React$useState7, 2),
-        startMs = _React$useState8[0],
-        setStartMs = _React$useState8[1];
+        assignmentId = _React$useState8[0],
+        setAssignmentId = _React$useState8[1];
+
+    var _React$useState9 = React.useState(new Date()),
+        _React$useState10 = _slicedToArray(_React$useState9, 2),
+        beginHit = _React$useState10[0],
+        _ = _React$useState10[1];
+
+    var _React$useState11 = React.useState(new Date()),
+        _React$useState12 = _slicedToArray(_React$useState11, 2),
+        startMs = _React$useState12[0],
+        setStartMs = _React$useState12[1];
     // fetch from API and update later on => set to 1 if all trials finished, set to 2 if ...
 
 
-    var _React$useState9 = React.useState(2),
-        _React$useState10 = _slicedToArray(_React$useState9, 2),
-        statusCode = _React$useState10[0],
-        setStatusCode = _React$useState10[1];
+    var _React$useState13 = React.useState(2),
+        _React$useState14 = _slicedToArray(_React$useState13, 2),
+        statusCode = _React$useState14[0],
+        setStatusCode = _React$useState14[1];
 
-    var _React$useState11 = React.useState(),
-        _React$useState12 = _slicedToArray(_React$useState11, 2),
-        selection = _React$useState12[0],
-        setSelection = _React$useState12[1];
+    var _React$useState15 = React.useState(),
+        _React$useState16 = _slicedToArray(_React$useState15, 2),
+        selection = _React$useState16[0],
+        setSelection = _React$useState16[1];
 
     var QUESTIONS_PP = 6;
+
+    var handlePagination = function handlePagination(move) {
+      if (pageNo + move >= 0 & pageNo + move < pages.length) {
+        setPageNo(pageNo + move);
+      } else {
+        console.log("Won't do nothing.");
+      }
+    };
 
     var downloadSurveyData = function downloadSurveyData(projectId) {
       fetch(SERVER_URL + "/get-surveys-by-project-id/" + projectId, {
@@ -59,10 +77,33 @@ var Survey = function Survey(_ref) {
       });
     };
 
-    // runs once at the beginning of the assignment
+    // runs only once at the beginning
     React.useEffect(function () {
       downloadSurveyData(projectId);
     }, []);
+
+    // runs when survey data changes
+    React.useEffect(function () {
+      if (survey !== undefined) {
+        var i = 0;
+        var j = void 0;
+        var nItems = 0;
+        var nPages = void 0;
+        var len = survey.length;
+        var pagesTmp = [];
+        for (; i < len;) {
+          nItems = survey[i]["items"].length;
+          nPages = Math.ceil(nItems / QUESTIONS_PP);
+          j = 0;
+          for (; j < nPages;) {
+            pagesTmp.push([i, QUESTIONS_PP * j]);
+            j++;
+          }
+          i++;
+        }
+        setPages(pagesTmp);
+      }
+    }, [survey]);
 
     // rendering
     return React.createElement(
@@ -72,31 +113,31 @@ var Survey = function Survey(_ref) {
         "div",
         { className: "container" },
         React.createElement(
+          "h1",
+          null,
+          pages && survey[pages[pageNo][0]]["name"]
+        ),
+        React.createElement(
           "div",
           null,
-          survey && survey.map(function (s) {
-            return s && React.createElement(
+          pages && survey[pages[pageNo][0]]["items"].slice(pages[pageNo][1], pages[pageNo][1] + QUESTIONS_PP).map(function (item) {
+            return React.createElement(
               "div",
               null,
               React.createElement(
-                "h1",
+                "span",
                 null,
-                s.name
+                item
               ),
-              s.items && s.items.map(function (item) {
-                return item && React.createElement(
-                  "div",
-                  null,
-                  React.createElement(
-                    "span",
-                    null,
-                    item
-                  ),
-                  React.createElement(ResponseBox, { s: s, item: item })
-                );
+              React.createElement(ResponseBox, {
+                s: survey[pages[pageNo][0]],
+                item: item
               })
             );
-          })
+          }),
+          1 + pageNo,
+          "/",
+          pages && pages.length
         ),
         React.createElement(
           "div",
@@ -106,10 +147,27 @@ var Survey = function Survey(_ref) {
             { className: "submit-button-tile" },
             React.createElement(
               "button",
-              { onClick: function onClick(e) {
-                  return handleClick(e);
-                } },
-              "Proceed"
+              {
+                disabled: pageNo === 0,
+                onClick: function onClick() {
+                  return handlePagination(-1);
+                }
+              },
+              "Back"
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "submit-button-tile" },
+            React.createElement(
+              "button",
+              {
+                disabled: pages && pageNo == pages.length - 1,
+                onClick: function onClick() {
+                  return handlePagination(1);
+                }
+              },
+              "Next"
             )
           )
         )
@@ -131,10 +189,10 @@ var ResponseBox = function ResponseBox(_ref2) {
         justifyContent: "space-between",
         alignItems: "flex-end",
         marginTop: "1rem",
-        marginBottom: "3rem"
+        marginBottom: "2rem"
       }
     },
-    s.scale.map(function (likert) {
+    s.scale && s.scale.map(function (likert) {
       return likert && React.createElement(
         "div",
         {
