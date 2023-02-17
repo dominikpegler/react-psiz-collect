39b1f8a98385dc19eaa4cd738c92471f4f7d3d6a
dominikpegler <dinkopeg@posteo.net>
2023-02-17 13:23:40 +0100

    change directory name

--- backend/survey.py
+++ backend/survey.py
@@ -3,21 +3,20 @@ import json
 import psycopg2
 
 
-def table_exists(engine,name):
+def table_exists(engine, name):
     ins = inspect(engine)
-    ret = ins.dialect.has_table(engine.connect(),name)
+    ret = ins.dialect.has_table(engine.connect(), name)
     return ret
 
 
 def write_survey_to_db(project_id, assignment_id, selection, DATABASE_URL):
 
-
     engine = create_engine(DATABASE_URL)
     metadata = MetaData(engine)
 
-    with open(f"api/projects/{project_id}.json") as fp:
+    with open(f"backend/projects/{project_id}.json") as fp:
         res = json.load(fp)
-        with open("api/projects/surveys/" + res['surveys'][0] + ".json") as fp2:
+        with open("backend/projects/surveys/" + res["surveys"][0] + ".json") as fp2:
             survey = json.load(fp2)
 
     columns = list(survey["items"])
@@ -28,20 +27,30 @@ def write_survey_to_db(project_id, assignment_id, selection, DATABASE_URL):
     columns = [table_name + "_" + i.zfill(n_chars) for i in columns]
     columns = [Column(i, Integer) for i in columns]
 
-    selection = {table_name+"_" + key.zfill(n_chars): int(val) for key, val in selection.items()}
+    selection = {
+        table_name + "_" + key.zfill(n_chars): int(val)
+        for key, val in selection.items()
+    }
     new_survey_data = {"assignment_id": assignment_id, **selection}
 
-    if not table_exists(engine, table_name): # table does not exist, so we will create it
-        Table(table_name, metadata, Column("Id", Integer, primary_key=True, nullable=False), Column("assignment_id", Integer), *columns)
+    if not table_exists(
+        engine, table_name
+    ):  # table does not exist, so we will create it
+        Table(
+            table_name,
+            metadata,
+            Column("Id", Integer, primary_key=True, nullable=False),
+            Column("assignment_id", Integer),
+            *columns,
+        )
         print(metadata)
-        metadata.create_all()    
-
+        metadata.create_all()
 
     con = psycopg2.connect(DATABASE_URL)
     cur = con.cursor()
     sql_cols = str(tuple(new_survey_data.keys()))
     sql_vals = str(tuple(new_survey_data.values()))
-    sql = f'INSERT INTO {table_name} {sql_cols} VALUES {sql_vals}'
+    sql = f"INSERT INTO {table_name} {sql_cols} VALUES {sql_vals}"
     sql = sql.replace("'", "")
     cur.execute(sql)
     con.commit()
